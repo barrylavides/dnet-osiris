ee77fd924e4f9b06c57b042a97709372996c3275
Marvin <marvinamanteromagos@gmail.com>
2018-04-04 17:22:56 +0800

    Updated files

---
+++ assets/js/global-script.js
@@ -0,0 +1,16 @@
+//This is for the sidebar
+jQuery(document).ready(function($){
+
+  $( ".bookmarks a" ).click(function(event) {
+      event.preventDefault();
+  });
+  $( ".snapshot a" ).click(function(event) {
+      event.preventDefault();
+
+  });
+  $( ".history a" ).click(function(event) {
+      event.preventDefault();
+      $(".web-ui").addClass("default-widget-active");
+  });
+
+});
